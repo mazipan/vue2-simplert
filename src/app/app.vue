3b71493f93eff6bc5dc349a41cd182e6161590ad
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-05-12 16:50:50 +0700

    update demo

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,48 +1,81 @@
 
 <template>
-  <div>
+  <div class="grid__row">
+
+    <img src="https://mazipan.github.io/vue2-simplert/images/vue2-simplert-logo.png" 
+    alt="Vue2-Simplert" title="Vue2-Simplert" />
 
     <h1>Vue2-Simplert</h1>
     <h5>By Irfan Maulana</h5>
-    <div>
-      <button class="demo__btn open--info"
-              v-on:click="open('Information!', 'Hey, I am Opened...', 'info')">
-            Open Information Alert
-      </button>
 
-      <button class="demo__btn open--info"
-              v-on:click="open('Success!', 'Hey, I am Opened...', 'success')">
-            Open Success Alert
-      </button>
+    <a class="button button--radius button--blue"  href="https://github.com/mazipan/vue2-simplert"
+       target="_blank">
+        Download
+    </a>   
 
-      <button class="demo__btn open--error"
-              v-on:click="open('Error!', 'Hey, I am Opened...', 'error')">
-            Open Error Alert
-      </button>
+    <div class="grid__row">
+      <h2>Example</h2>
 
-      <button class="demo__btn open--warning"
-              v-on:click="open('Warning!', 'Hey, I am Opened...', 'warning')">
-            Open Warning Alert
-      </button>
+      <div class="grid__col-12">
+        <div class="example__title">Information Alert</div>
+        <button class="button button--radius button--blue"
+                v-on:click="open('Information!', 'Hey, I am Opened...', 'info')">
+              Click Me!
+        </button>        
+      </div>
 
-      <button class="demo__btn open--nontitle"
-              v-on:click="open('', 'Hey, I am Opened, but I dont have title', '')">
-            Open Alert Without Title
-      </button>
+      <div class="grid__col-12">
+        <div class="example__title">Success Alert</div>
+        <button class="button button--radius button--green"
+                v-on:click="open('Success!', 'Hey, I am Opened...', 'success')">
+              Click Me!
+        </button>        
+      </div>
 
-      <button class="demo__btn open--customcolor"
-              v-on:click="open('Custom Button', 'Hey, I am Opened...', '')">
-            Open Alert With Custom Button
-      </button>
+      <div class="grid__col-12">
+        <div class="example__title">Error Alert</div>
+        <button class="button button--radius button--red"
+                v-on:click="open('Error!', 'Hey, I am Opened...', 'error')">
+              Click Me!
+        </button>
+      </div>
+
+      <div class="grid__col-12">
+        <div class="example__title">Warning Alert</div>
+        <button class="button button--radius button--orange"
+                v-on:click="open('Warning!', 'Hey, I am Opened...', 'warning')">
+              Click Me!
+        </button>        
+      </div>
+
+      <div class="grid__col-12">
+        <div class="example__title">Alert Without Title</div>
+        <button class="button button--radius button--blue"
+                v-on:click="open('', 'Hey, I am Opened, but I dont have title', '')">
+              Click Me!
+        </button>        
+      </div>
+
+      <div class="grid__col-12">
+        <div class="example__title">Alert With Custom Button</div>
+        <button class="button button--radius button--blue"
+                v-on:click="open('Custom Button', 'Hey, I am Opened...', '')">
+              Click Me!
+        </button>        
+      </div>
     </div>
-    <br/>
 
-    <a href="https://github.com/mazipan/vue2-simplert"
-       target="_blank"
-       style="color: #068AC9; text-decoration: none;">
-      Fork on Github
-    </a>
+    <div class="grid__row">
+      <h2>Contribute</h2>
 
+      <span>
+        Feel free to fork Vue2-Simplert on <a href="https://github.com/mazipan/vue2-simplert"
+       target="_blank">GitHub</a> if you have any features or bugs!        
+      </span>
+
+    </div>
+
+    <!-- Vue2-Simplert DOM start -->
     <simplert isUseRadius=true
               isUseIcon=true
               :simplertData=simplertData>
@@ -75,16 +108,3 @@
   };
 </script>
 
-<style scoped>
-  .demo__btn{
-      padding: 10px 20px;
-      outline: none;
-      border: none;
-      text-align: center;
-      cursor: pointer;
-      color: #fff;
-      background-color: #068AC9;
-      -webkit-appearance: none;
-      -moz-appearance: none;
-  }
-</style>
