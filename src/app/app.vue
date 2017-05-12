ab0b2426180086a3106d4f2522b97bb10467db52
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-05-12 17:13:05 +0700

    update demo

--- src/app/app.vue
+++ src/app/app.vue
@@ -2,22 +2,24 @@
 <template>
   <div class="grid__row">
 
-    <img src="https://mazipan.github.io/vue2-simplert/images/vue2-simplert-logo.png" 
-    alt="Vue2-Simplert" title="Vue2-Simplert" />
+    <div class="header__title">      
+      <img src="https://mazipan.github.io/vue2-simplert/images/vue2-simplert-logo.png" 
+      alt="Vue2-Simplert" title="Vue2-Simplert" />
 
-    <h1>Vue2-Simplert</h1>
-    <h5>By Irfan Maulana</h5>
+      <h5 class="header__subtitle">Vue 2 Simple Alert Component (SweetAlert Inspired) By Irfan Maulana</h5>
+    </div>
 
-    <a class="button button--radius button--blue"  href="https://github.com/mazipan/vue2-simplert"
+
+    <a class="button button--radius button--blue btn-download"  href="https://github.com/mazipan/vue2-simplert"
        target="_blank">
         Download
     </a>   
 
-    <div class="grid__row">
+    <div class="grid__row content">
       <h2>Example</h2>
 
       <div class="grid__col-12">
-        <div class="example__title">Information Alert</div>
+        <div class="grid__col-3 example__title">Information Alert</div>
         <button class="button button--radius button--blue"
                 v-on:click="open('Information!', 'Hey, I am Opened...', 'info')">
               Click Me!
@@ -25,7 +27,7 @@
       </div>
 
       <div class="grid__col-12">
-        <div class="example__title">Success Alert</div>
+        <div class="grid__col-3 example__title">Success Alert</div>
         <button class="button button--radius button--green"
                 v-on:click="open('Success!', 'Hey, I am Opened...', 'success')">
               Click Me!
@@ -33,7 +35,7 @@
       </div>
 
       <div class="grid__col-12">
-        <div class="example__title">Error Alert</div>
+        <div class="grid__col-3 example__title">Error Alert</div>
         <button class="button button--radius button--red"
                 v-on:click="open('Error!', 'Hey, I am Opened...', 'error')">
               Click Me!
@@ -41,7 +43,7 @@
       </div>
 
       <div class="grid__col-12">
-        <div class="example__title">Warning Alert</div>
+        <div class="grid__col-3 example__title">Warning Alert</div>
         <button class="button button--radius button--orange"
                 v-on:click="open('Warning!', 'Hey, I am Opened...', 'warning')">
               Click Me!
@@ -49,7 +51,7 @@
       </div>
 
       <div class="grid__col-12">
-        <div class="example__title">Alert Without Title</div>
+        <div class="grid__col-3 example__title">Alert Without Title</div>
         <button class="button button--radius button--blue"
                 v-on:click="open('', 'Hey, I am Opened, but I dont have title', '')">
               Click Me!
@@ -57,7 +59,7 @@
       </div>
 
       <div class="grid__col-12">
-        <div class="example__title">Alert With Custom Button</div>
+        <div class="grid__col-3 example__title">Alert With Custom Button</div>
         <button class="button button--radius button--blue"
                 v-on:click="open('Custom Button', 'Hey, I am Opened...', '')">
               Click Me!
@@ -65,7 +67,7 @@
       </div>
     </div>
 
-    <div class="grid__row">
+    <div class="grid__row content centered">
       <h2>Contribute</h2>
 
       <span>
@@ -108,3 +110,39 @@
   };
 </script>
 
+
+<style lang="scss">
+.centered{
+  text-align: center;
+}
+.header__title{
+  text-align: center;
+  margin-top: 26px;
+}
+.header__subtitle{
+  font-size: 2rem;
+  font-style: italic;
+  color: #a6b6b9;
+  font-weight: 200;
+}
+.btn-download{  
+    position: fixed;
+    right: 30px;
+    top: 0;
+    font-weight: 500;
+    text-transform: uppercase;
+    z-index: 3;
+    margin: 26px 5px 0 5px;
+}
+.example__title{
+  margin-right: 2em;
+  font-size: 1.6rem;
+  color: #a6b6b9;
+}
+.grid__col-12{
+  margin: 20px 0;
+}
+.content{
+  margin: 20px 0;
+}
+</style>
