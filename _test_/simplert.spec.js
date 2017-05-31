bc044efb3e97f815402ab3e5a305afc68789f890
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-05-31 16:41:14 +0700

    init test

---
+++ _test_/simplert.spec.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import sinon from 'sinon'
+import Simplert from '../src/app/simplert.vue'
+
+describe('Simplert', () => {
+  let app
+  beforeEach(() => {
+    app = new Vue({
+      template: `<div>
+										<simplert useRadius=true
+                              useIcon=true
+                              ref="simplert">
+                    </simplert>
+								 </div>`,
+      components: {
+        Simplert
+      }
+    }).$mount()
+  })
+
+  afterEach(() => {
+    app.$destroy()
+    app = null
+  })
+
+})
